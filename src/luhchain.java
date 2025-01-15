4f32255f1a96ac6e7d4c7cfd11ece8716f2a30d8
truman <trumanzhe@gmail.com>
2025-01-15 11:48:56 -0500

    lil

--- src/luhchain.java
+++ src/luhchain.java
@@ -1,15 +1,17 @@
+package luhcoin;
+
 public class luhchain {
 
 	public static void main(String[] args) {
 
-		Block genesisBlock = new Block("Luh geeky's first block yuh", "0");
-		System.out.println("hash for block 1 : " + genesisBlock.hash);
+		Block genesisBlock = new Block("Hi im the first block", "0");
+		System.out.println("Hash for block 1 : " + genesisBlock.hash);
 		
-		Block secondBlock = new Block("Mean feen ft kranky kranky",genesisBlock.hash);
-		System.out.println("hash for block 2 : " + secondBlock.hash);
+		Block secondBlock = new Block("Yo im the second block",genesisBlock.hash);
+		System.out.println("Hash for block 2 : " + secondBlock.hash);
 		
-		Block thirdBlock = new Block("Nvr again",secondBlock.hash);
-		System.out.println("hash for block 3 : " + thirdBlock.hash);
+		Block thirdBlock = new Block("Hey im the third block",secondBlock.hash);
+		System.out.println("Hash for block 3 : " + thirdBlock.hash);
 	}
 
 }
